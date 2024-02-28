fcee0daa7dc96b57e8a0e04792dd587ba9c35e02
keshav <keshavkoirala933@gmail.com>
2024-02-28 19:04:34 +0545

    Initial commit

---
+++ nm.c/trapejoidal.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+float f(float x)
+{
+    return(1/x);
+}
+void main()
+{
+    float a,b,h,s=0,s1;
+    int i,n;
+    printf("enter no of imterval:")
+}
